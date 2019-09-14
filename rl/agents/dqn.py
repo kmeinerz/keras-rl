ce348b6144db78d83dda84a1bda56d28b71f678a
Kai Meinerz <kmeinerz@thp.uni-koeln.de>
2019-09-15 01:06:41 +0200

    tgzklölkjhgfth

--- rl/agents/dqn.py
+++ rl/agents/dqn.py
@@ -256,11 +256,9 @@ class DQNAgent(AbstractDQNAgent):
         if self.step % self.memory_interval == 0:
             if self.enable_multi_observation:
                 self.memory.append(self.recent_observation[self.action//self.nb_actions,:,:], self.recent_action%self.nb_actions, reward, terminal,
-                                training=self.training) reward, terminal,
                                 training=self.training)
             else:
                 self.memory.append(self.recent_observation, self.recent_action, reward, terminal,
-                                training=self.training) reward, terminal,
                                 training=self.training)
 
         metrics = [np.nan for _ in self.metrics_names]
